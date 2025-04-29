c02c9f519121cc749733d482d38efd79154f93ad
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-04-29 11:00:39 -0300

    feat(auth): implement login functionality with JWT authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 import userModel from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+const { JsonWebTokenError } = jwt;
 
 class AuthController {
   // Listar Todos os Usuários
@@ -53,6 +55,56 @@ class AuthController {
     } catch (error) {
       console.error("Erro ao criar novo usuário:", error);
       res.status(500).json({ error: `Erro ao criar usuário ${name}` });
+    }
+  }
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+  
+      // Validação Básica
+      if (!email || !password) {
+        return res.status(400).json({
+          error: "Os campos: email e password são obrigatórios!!",
+        });
+      }
+  
+      // Verificar se o Usuário Existe
+      const userExists = await userModel.findByEmail(email);
+  
+      if (!userExists) {
+        return res.status(401).json({ error: "Credenciais erradas!" });
+      }
+  
+      // Verificar Senha
+      const isPasswordValid = await bcryptjs.compare(password, userExists.password);
+  
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Credenciais erradas!" });
+      }
+  
+      // Gerar Token JWT
+      const token = jwt.sign(
+        {
+          id: userExists.id,
+          name: userExists.name,
+          email: userExists.email,
+        },
+        process.env.JWT_SECRET, // Certifique-se de que JWT_SECRET está definido
+        {
+          expiresIn: "24h",
+        }
+      );
+  
+      // Retornar o token e mensagem de sucesso
+      return res.json({
+        message: `Olá, ${userExists.name}! Seu login foi realizado com sucesso!`,
+        token,
+        user: userExists,
+      });
+    } catch (error) {
+      console.error("Erro ao fazer login:", error);
+      res.status(500).json({ error: "Erro ao fazer login" }); // Alterado para 500 Internal Server Error
     }
   }
 }
